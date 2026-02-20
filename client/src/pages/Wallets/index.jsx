c4762383bfd89fa450df43ae57e22b85faa3d584
tiru0067 <tiru0067@gmail.com>
2026-02-20 10:28:01 +0530

    feat(wallets): Add WalletOverview and refractor layout
    - Introduce WalletOverview component for wallet summary disply
    - Remove reusable section component
    - Replace section with semantic section elements
    - Move wallet header

--- client/src/pages/Wallets/index.jsx
+++ client/src/pages/Wallets/index.jsx
@@ -1,49 +1,34 @@
 import WalletList from "../../components/wallets/WalletList";
-
-const Section = ({
-  title,
-  buttonText,
-  onClick,
-  className = "",
-  btnClassName = "btn btn-secondary",
-  children,
-}) => {
-  return (
-    <section className={`max-md:mb-6 ${className}`}>
-      <div className="mb-3 flex justify-between items-center">
-        <h3 className="font-medium text-base text-neutral-500 dark:text-neutral-500 truncate">
-          {title}
-        </h3>
-        <button type="button" className={btnClassName} onClick={onClick}>
-          {buttonText}
-        </button>
-      </div>
-
-      {children}
-    </section>
-  );
-};
+import WalletOverview from "../../components/wallets/WalletOverview";
 
 const Wallets = () => {
   return (
     <div className="w-full h-full flex max-md:flex-col gap-9 overflow-y-auto hide-scrollbar">
       {/* Wallet Overview */}
-      <Section
-        title={"Overview"}
-        className="md:w-2/5 p-5 h-fit rounded-xl bg-neutral-100 dark:bg-neutral-900"
-        btnClassName="hidden"
-      >
-        <p>Wallet Overview</p>
-      </Section>
+      <section className="max-md:mb-6 md:w-2/5 p-5 h-fit rounded-xl bg-neutral-100 dark:bg-neutral-900">
+        <WalletOverview />
+      </section>
 
       {/* Wallets */}
-      <Section
-        title={"Your Wallets"}
-        buttonText={"Add Wallet"}
-        className="md:w-3/5 mb-8"
-      >
+      <section className="max-md:mb-6 md:w-3/5 mb-8">
+        {/* Header */}
+        <div className="mb-3 flex justify-between items-center">
+          <h3 className="font-medium text-base text-neutral-500 dark:text-neutral-500 truncate">
+            Your wallets
+          </h3>
+
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => {}}
+          >
+            Add Wallet
+          </button>
+        </div>
+
+        {/* Wallet list */}
         <WalletList />
-      </Section>
+      </section>
     </div>
   );
 };
